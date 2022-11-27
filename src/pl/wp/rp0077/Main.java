f225efc2b5f5fc5f3bf3d071cca174d494247881
Ryszard Piechowiak <piechowiak.ryszard@gmail.com>
2022-11-28 00:19:33 +0100

    Basic classes created

--- src/pl/wp/rp0077/Main.java
+++ src/pl/wp/rp0077/Main.java
@@ -1,7 +1,12 @@
 package pl.wp.rp0077;
 
+import pl.wp.rp0077.transport.CargoTransport;
+import pl.wp.rp0077.transport.TransportFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CargoTransport cargoTransport = TransportFactory.getTransport("Truck");
+
+        cargoTransport.process();
     }
 }
